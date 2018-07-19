bc87aab9add61e188542e6df7e7a799437d61cc1
Shaurita <ms.s.hutchins@gmail.com>
2018-07-18 20:33:38 -0500

    Create schema.js

---
+++ js/schema.js
@@ -0,0 +1,7 @@
+{
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "name": "Datasnakes",
+  "url": "https://www.datasnakes.org/",
+  "image": "https://www.datasnakes.org/img/logo.png"
+}
